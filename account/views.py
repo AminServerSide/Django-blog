2a1eab04adb63a77cb102e473e99ee1dcc8bd546
MohammadAmin <a.normohammadi83@gmail.com>
2025-03-23 17:18:46 +0330

    create logout

--- account/views.py
+++ account/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login , logout
 
 
 def user_login(request):
-    if request.method == "POST":
+    if request.user.is_authenticated == True:
+        return redirect('/')
+
+
+    if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
@@ -11,3 +15,7 @@ def user_login(request):
             login(request, user)
             return redirect('/')
     return render(request, 'account/login.html' , {})
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
