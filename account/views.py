ee83860194aee625c944fa25e0d606e78b2247ac
MohammadAmin <a.normohammadi83@gmail.com>
2025-03-23 19:54:17 +0330

    create register page

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login , logout
 
@@ -15,6 +16,28 @@ def user_login(request):
             login(request, user)
             return redirect('/')
     return render(request, 'account/login.html' , {})
+
+
+def user_register(request):
+    context = {"errors": []}  # Fix the key name
+    if request.user.is_authenticated == True:
+        return redirect('/')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            context['errors'].append("Passwords don't match")
+            return render(request, 'account/register.html', context)
+
+        user =  User.objects.create_user(username=username, email=email, password=password1)
+        login(request, user)
+        return redirect('/')
+
+    return render(request , 'account/register.html' , {})
+
 
 def user_logout(request):
     logout(request)
