2986ea386c537b2e6c97b93468f2475f6a37c9c2
MohammadAmin <a.normohammadi83@gmail.com>
2025-03-26 01:37:19 +0330

    creating model of category

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 class Article(models.Model):
-    author = models.ForeignKey(User , on_delete=models.CASCADE)
+    author = models.ForeignKey(User , on_delete=models.SET_NULL, null=True , blank=True)
     title = models.CharField(max_length=80)
+    category = models.ManyToManyField(Category)
     body = models.TextField()
     image = models.ImageField(upload_to='images/articles')
     created = models.DateTimeField(auto_now_add=True)
@@ -12,3 +22,5 @@ class Article(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.body[:30]}"
+
+
