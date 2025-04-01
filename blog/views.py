3703ef5a94411c1e9f20816eb723db6a3108c87b
MohammadAmin <a.normohammadi83@gmail.com>
2025-04-01 17:13:02 +0330

    adding paginations

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render , get_object_or_404
 from blog.models import Article , Category
+from django.core.paginator import Paginator
 
 def article_detail(request, slug):
-    article = get_object_or_404(Article , slug=slug)
+    article = get_object_or_404(Article, slug=slug)
     return render(request, 'blog/article_details.html', {'article':article})
 
 
 def article_list(request):
     articles = Article.objects.all()
-    return render(request , "blog/articles_list.html" , {'articles':articles})
+    page_number = request.GET.get('page')
+    paginator = Paginator(articles, 1)
+    objects_list = paginator.get_page(page_number)
+    return render(request , "blog/articles_list.html" , {'articles':objects_list})
 
 def category_detail(request , pk=None):
     category = get_object_or_404(Category, id=pk)
