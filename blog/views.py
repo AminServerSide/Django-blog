95c9ae203b9c7808da065c6c8c707d6894754553
MohammadAmin <a.normohammadi83@gmail.com>
2025-04-03 02:07:09 +0330

    add rpley comment

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render , get_object_or_404
-from blog.models import Article , Category
+from blog.models import Article , Category , Comment
 from django.core.paginator import Paginator
 
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
-    return render(request, 'blog/article_detail.html', {'article':article})
 
+    if request.method == 'POST':
+        parent_id = request.POST.get('parent_id', None)
+        body = request.POST.get('body')
+
+        # Convert parent_id only if it's not empty
+        parent_id = int(parent_id) if parent_id and parent_id.isdigit() else None
+
+        Comment.objects.create(body=body, article=article, user=request.user, parent_id=parent_id)
+
+    return render(request, 'blog/article_detail.html', {'article': article})
 
 def article_list(request):
     articles = Article.objects.all()
