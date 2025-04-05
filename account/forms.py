fda2368a1bcd3e663301e8a3589d037a457abd23
MohammadAmin <a.normohammadi83@gmail.com>
2025-04-06 01:50:14 +0330

    create edit user page

--- account/forms.py
+++ account/forms.py
@@ -1,14 +1,35 @@
 from django import forms
 from django.contrib.auth import authenticate
 from django.forms import ValidationError
+from django.contrib.auth.models import User
+
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'input100'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input100'}))
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'input100'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'input100'})
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise ValidationError("Username or password is incorrect", code="invalid_login")
+
+        return cleaned_data
 
 
-    def clean_username(self):
-        user = authenticate(username=self.cleaned_data.get('username') , password=self.cleaned_data.get('password'))
-        if user is not None:
-            return self.cleaned_data.get('password')
-        raise ValidationError("username or password is wrong" , code="invalid_info")
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email','first_name','last_name')
+
--- account/views.py
+++ account/views.py
@@ -1,23 +1,27 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login , logout
-from .forms import LoginForm
+from .forms import LoginForm , UserEditForm
 
 
 def user_login(request):
-    if request.user.is_authenticated == True:
+    if request.user.is_authenticated:
         return redirect('home_app:home')
-
 
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            user = User.objects.get(username=form.cleaned_data.get('username'))
-            login(request, user)
-            return redirect('home_app:home')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect('home_app:home')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html' , {'form':form})
+
+    return render(request, 'account/login.html', {'form': form})
 
 
 def user_register(request):
@@ -44,3 +48,15 @@ def user_register(request):
 def user_logout(request):
     logout(request)
     return redirect('home_app:home')
+
+
+def user_edit(request):
+    user = request.user
+    form = UserEditForm(instance=user)
+    if request.method == "POST":
+        form = UserEditForm(instance=user , data=request.POST)
+        if form.is_valid():
+            form.save()
+
+
+    return render(request, 'account/edit.html' , {'form':form})
